32e6676902fe3a7424087c11024ac94333424395
MdSayemMottakee <md.sayem.mottakee@g.bracu.ac.bd>
2024-04-24 20:43:43 +0600

    CSE370

--- login.php
+++ login.php
@@ -1,6 +1,12 @@
 <?php
 session_start();
 include('includes/config.php');
+// Establish database connection
+$conn = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
 
 if(isset($_POST['login'])) 
 {
